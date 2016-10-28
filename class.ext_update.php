50db25f7bb29307e982e725ae44a4ccefb77e914
Daniel Siepmann <coding@daniel-siepmann.de>
2016-10-28 13:52:21 +0200

    CLEANUP: Fix further PSR issues

--- class.ext_update.php
+++ class.ext_update.php
@@ -1,37 +1,29 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2012 Ivan Kartolo <ivan at kartolo dot de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
-
-/**
- * Class for updating Direct Mail to version 3
+ *  Copyright notice
  *
- * @author      Ivan Kartolo <ivan at kartolo dot de>
- * @package     TYPO3
- * @subpackage  tx_directmail
- */
+ *  (c) 2012 Ivan Kartolo <ivan at kartolo dot de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
@@ -40,27 +32,36 @@ use Ameos\AmeosFilemanager\Tools\Tools;
 
 class ext_update
 {
-
     /**
      * Main function, returning the HTML content of the module
      *
      * @return  string      HTML
      */
-    function main()
+    public function main()
     {
         if (!GeneralUtility::_GP('do_initialize')) {
             $content = $this->displayWarning();
-            $onClick = "document.location='".GeneralUtility::linkThisScript(array('do_initialize'=>1))."'; return false;";
+            $onClick = "document.location='"
+                . GeneralUtility::linkThisScript(array('do_initialize'=>1))
+                . "'; return false;"
+            ;
             $content .= htmlspecialchars($GLOBALS['LANG']->getLL('update_convert_now')).'
                 <br /><br />
-                <form action=""><input type="submit" value="'.LocalizationUtility::translate('doInitialize', 'ameos_filemanager').'" onclick="'.htmlspecialchars($onClick).'"></form>
-            ';
+                <form action=""><input type="submit" value="'
+                . LocalizationUtility::translate('doInitialize', 'ameos_filemanager')
+                . '" onclick="' . htmlspecialchars($onClick) . '"></form>'
+            ;
         } else {
             $this->countFolder = 0;
             $this->countAddedFolder = 0;
 
-            $updated = $this->initializeDatabase();
-            $content .= $updated.'<br/>'.LocalizationUtility::translate('initializeCountFolder', 'ameos_filemanager').' : '.$this->countFolder. ' - ' . $this->countAddedFolder .'<br/>'.LocalizationUtility::translate('adviceInitialize', 'ameos_filemanager');
+            $updated = $this->update();
+            $content .= $updated . '<br/>'
+                . LocalizationUtility::translate('initializeCountFolder', 'ameos_filemanager')
+                . ' : ' . $this->countFolder . ' - '
+                . $this->countAddedFolder . '<br/>'
+                . LocalizationUtility::translate('adviceInitialize', 'ameos_filemanager')
+            ;
 
         }
         return $content;
@@ -71,9 +72,9 @@ class ext_update
      *
      * @return  boolean     true if user have access, otherwise false
      */
-    function access()
+    public function access()
     {
-            // We cannot update before the extension is installed: required tables are not yet in TCA
+        // We cannot update before the extension is installed: required tables are not yet in TCA
         if (ExtensionManagementUtility::isLoaded('ameos_filemanager')) {
             return true;
         } else {
@@ -82,18 +83,15 @@ class ext_update
     }
 
     /**
-     * Go through the DB and initialize the basic settings to use the extension.
+     * Execute the update.
      *
-     * @return  string  indication about the success or failure of the task
+     * @return  string
      */
-    function initializeDatabase()
+    protected function update()
     {
-        $contenu = '';
         $this->objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
         $storageRepository = $this->objectManager->get('TYPO3\\CMS\\Core\\Resource\\StorageRepository');
-        $storages = $storageRepository->findAll();
-        $i=0;
-        foreach ($storages as $storage) {
+        foreach ($storageRepository->findAll() as $storage) {
             $this->currentStorage = $storage;
             $storagePath = PATH_site . '/' . $storage->getConfiguration()['basePath'];
             $this->setDatabaseForFolder(substr($storagePath, 0, -1), $storagePath);
@@ -102,20 +100,22 @@ class ext_update
     }
 
     /**
-     *
-     * Display the description of the task
      * @return string
      */
-    function displayWarning()
+    protected function displayWarning()
     {
         $out = '
             <div style="padding:15px 15px 20px 0;">
                 <div class="typo3-message message-warning">
-                        <div class="message-header">'.LocalizationUtility::translate('warning', 'ameos_filemanager').'</div>
-                        <div class="message-body">'.LocalizationUtility::translate('warningInitialize', 'ameos_filemanager').'</div>
-                    </div>
+                    <div class="message-header">'
+                        . LocalizationUtility::translate('warning', 'ameos_filemanager')
+                    . '</div>
+                    <div class="message-body">'
+                        . LocalizationUtility::translate('warningInitialize', 'ameos_filemanager')
+                    . '</div>
                 </div>
-            </div>';
+            </div>'
+        ;
 
         return $out;
     }
@@ -128,17 +128,23 @@ class ext_update
      * @param integer $uidParent his parent's uid
      * @return string
      */
-    function setDatabaseForFolder($storageFolderPath, $folder, $uidParent = 0)
+    protected function setDatabaseForFolder($storageFolderPath, $folder, $uidParent = 0)
     {
         $this->countFolder ++;
         if ($handle = opendir($folder)) {
             while (false !== ($entry = readdir($handle))) {
                 if ($entry != "." && $entry != "..") {
                     if (is_dir($folder . $entry)) {
-                        $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery("uid", "tx_ameosfilemanager_domain_model_folder", "tx_ameosfilemanager_domain_model_folder.title like '". $entry ."'");
+                        $res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+                            "uid",
+                            "tx_ameosfilemanager_domain_model_folder",
+                            "tx_ameosfilemanager_domain_model_folder.title like '". $entry ."'"
+                        );
                         $exist = false;
                         while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
-                            if (Tools::getFolderPathFromUid($row['uid']) == str_replace($storageFolderPath, '', $folder . $entry)) {
+                            if (Tools::getFolderPathFromUid($row['uid'])
+                                == str_replace($storageFolderPath, '', $folder . $entry)
+                            ) {
                                 $exist = true;
                                 $uid = $row['uid'];
                                 break;
@@ -175,14 +181,18 @@ class ext_update
         }
     }
 
-    function setDatabaseForFile($storageFolderPath, $folderPath, $entry, $folderParentUid)
+    protected function setDatabaseForFile($storageFolderPath, $folderPath, $entry, $folderParentUid)
     {
         $filePath = $folderPath . $entry;
         $fileIdentifier = str_replace($storageFolderPath, '', $filePath);
         $infoFile = $this->currentStorage->getFileInfoByIdentifier($fileIdentifier, array());
 
         // Add file into sys_file if it doesn't exist
-        $file = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('uid', 'sys_file', "identifier LIKE '" . $fileIdentifier . "'");
+        $file = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow(
+            'uid',
+            'sys_file',
+            "identifier LIKE '" . $fileIdentifier . "'"
+        );
         if ($file) {
             $fileUid = $file['uid'];
 
@@ -191,7 +201,7 @@ class ext_update
                 'sys_file_metadata',
                 'sys_file_metadata.file = '.$fileUid,
                 array('folder_uid' => $folderParentUid),
-                $no_quote_fields = false
+                false
             );
         } else {
             $GLOBALS['TYPO3_DB']->exec_INSERTquery(
